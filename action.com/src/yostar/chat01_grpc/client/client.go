e60ec8ab51d30980d1d93b9753e0362dc1b09eed
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-03-20 23:32:22 +0800

    grpc

--- action.com/src/yostar/chat01_grpc/client/client.go
+++ action.com/src/yostar/chat01_grpc/client/client.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
+	"time"
 	"yostar/chat01_grpc/proto"
-
-	"github.com/name5566/leaf/log"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -17,6 +18,17 @@ var (
 	serverAddr         = flag.String("server_addr", "127.0.0.1:10000", "<Host Addr>")
 	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "<- ->")
 )
+
+func printFeature(client proto.RouteGuideClient, point *proto.Point) {
+	log.Printf("Getting feature for point (%d,%d)", point.Latitude, point.Longitude)
+	ctx, ctf := context.WithTimeout(context.Background(), 10*time.Second)
+	defer ctf()
+	if feature, err := client.GetFeature(ctx, point); err != nil {
+		log.Fatalf("%v . GetFeatures() = , _ , %v \n", client, err)
+	} else {
+		log.Println(feature)
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -31,16 +43,17 @@ func main() {
 			*caFile = testdata.Path("ca.pem")
 		}
 		if creds, err = credentials.NewClientTLSFromFile(*caFile, *serverHostOverride); err != nil {
-			log.Fatal("Failed to create TLS err: %v", err)
+			log.Fatalf("Failed to create TLS err: %v", err)
 		}
 		opts = append(opts, grpc.WithTransportCredentials(creds))
 	} else {
 		opts = append(opts, grpc.WithInsecure())
 	}
 	if conn, err = grpc.Dial(*serverAddr, opts...); err != nil {
-		log.Fatal("fail to dail:%v ", err)
+		log.Fatalf("fail to dail:%v ", err)
 	}
 	defer conn.Close()
 	client := proto.NewRouteGuideClient(conn)
+	printFeature(client, &proto.Point{Latitude: 409146138, Longitude: -746188906})
 
 }
