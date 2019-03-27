f71ab20d3670cc9be2d837e27dddf536252fc1c6
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-03-27 23:57:50 +0800

    codec

--- action.com/src/yostar/chat06_simplerpc/server/codec.go
+++ action.com/src/yostar/chat06_simplerpc/server/codec.go
@@ -1,5 +1,10 @@
 package server
 
+import (
+	"bytes"
+	"encoding/gob"
+)
+
 //gob编解码
 
 type RpcData struct {
@@ -8,9 +13,26 @@ type RpcData struct {
 }
 
 func encode(data RpcData) ([]byte, error) {
-
+	var (
+		buf bytes.Buffer
+		err error
+	)
+	bufEncode := gob.NewEncoder(&buf)
+	if err = bufEncode.Encode(data); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 func decode(b []byte) (RpcData, error) {
-
+	var (
+		data RpcData
+		err  error
+	)
+	buf := bytes.NewBuffer(b)
+	bufDecode := gob.NewDecoder(buf)
+	if err = bufDecode.Decode(&data); err != nil {
+		return RpcData{}, err
+	}
+	return data, nil
 }
