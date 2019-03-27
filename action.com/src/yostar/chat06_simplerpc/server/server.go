365fdd654b5871ce9123e33acc6c681daf347aad
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-03-28 00:12:31 +0800

    rpc_server

--- action.com/src/yostar/chat06_simplerpc/server/server.go
+++ action.com/src/yostar/chat06_simplerpc/server/server.go
@@ -17,10 +17,14 @@ func NewRpcServer(address string) *RpcServer {
 
 func (rpcSev *RpcServer) Run() {
 	var (
-		listen net.Listener
-		conn   net.Conn
-		err    error
-		bytes  []byte
+		listen    net.Listener
+		conn      net.Conn
+		err       error
+		bytes     []byte
+		respBytes []byte
+		rpcData   RpcData
+		funcName  reflect.Value
+		ok        bool
 	)
 	if listen, err = net.Listen("tcp", rpcSev.addr); err != nil {
 		panic(err)
@@ -35,6 +39,37 @@ func (rpcSev *RpcServer) Run() {
 			log.Printf("SessReadErr:%v \n", err)
 			continue
 		}
+		//解码
+		if rpcData, err = decode(bytes); err != nil {
+			log.Printf("DecodeErr:%v \n", err)
+			continue
+		}
+		if funcName, ok = rpcSev.funds[rpcData.Name]; !ok {
+			log.Printf("Func %s not exists!\n", rpcData.Name)
+			continue
+		}
+		//构造参数
+		inArgs := make([]reflect.Value, 0, len(rpcData.Args))
+		for _, arg := range rpcData.Args {
+			inArgs = append(inArgs, reflect.ValueOf(arg))
+		}
+		//函数调用
+		out := funcName.Call(inArgs)
+		outArgs := make([]interface{}, 0, len(out))
+		for _, o := range out {
+			outArgs = append(outArgs, o.Interface())
+		}
+		//返回数据
+		respData := RpcData{Name: rpcData.Name, Args: outArgs}
+		if respBytes, err = encode(respData); err != nil {
+			log.Printf("EncodeErr:%v \n", err)
+			continue
+		}
 
+		if err = session.Write(respBytes); err != nil {
+			log.Printf("SendErr:%v \n", err)
+			continue
+		}
+		log.Printf("SendSuccessful Data!\n")
 	}
 }
