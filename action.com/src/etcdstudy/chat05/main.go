7865326681a074c01db4530a3426569e88033426
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-04-07 23:07:41 +0800

    feat:put & get

--- action.com/src/etcdstudy/chat05/main.go
+++ action.com/src/etcdstudy/chat05/main.go
@@ -8,15 +8,13 @@ import (
 	"go.etcd.io/etcd/clientv3"
 )
 
-//向etcd写入数据
+//etcd GET
 
 func main() {
-
 	var (
 		client *clientv3.Client
 		err    error
 	)
-
 	config := clientv3.Config{
 		Endpoints:   []string{":2379"},
 		DialTimeout: 5 * time.Second,
@@ -26,17 +24,15 @@ func main() {
 		fmt.Print(err)
 		return
 	}
-	//拿到kv存储引擎
+
 	dataEngine := clientv3.NewKV(client)
 
-	//PUT
-	if putResp, err := dataEngine.Put(context.TODO(), "/cron/jobs/job1", "running", clientv3.WithPrevKV()); err != nil {
+	//GET
+	if getResp, err := dataEngine.Get(context.TODO(), "/cron/jobs/job1" /*clientv3.WithCountOnly()*/); err != nil {
 		fmt.Print(err)
 		return
 	} else {
-		fmt.Println("Revision:", putResp)
-		if putResp.PrevKv != nil {
-			fmt.Println("Prevalue:", string(putResp.PrevKv.Value))
-		}
+		fmt.Println(getResp.Kvs, " * ", getResp.Count)
 	}
+
 }
--- action.com/src/etcdstudy/chat06/main.go
+++ action.com/src/etcdstudy/chat06/main.go
@@ -8,15 +8,13 @@ import (
 	"go.etcd.io/etcd/clientv3"
 )
 
-//向etcd写入数据
+//GET
 
 func main() {
-
 	var (
 		client *clientv3.Client
 		err    error
 	)
-
 	config := clientv3.Config{
 		Endpoints:   []string{":2379"},
 		DialTimeout: 5 * time.Second,
@@ -26,17 +24,16 @@ func main() {
 		fmt.Print(err)
 		return
 	}
-	//拿到kv存储引擎
+
 	dataEngine := clientv3.NewKV(client)
 
-	//PUT
-	if putResp, err := dataEngine.Put(context.TODO(), "/cron/jobs/job1", "running", clientv3.WithPrevKV()); err != nil {
+	if getResp, err := dataEngine.Get(context.TODO(), "/cron/jobs/", clientv3.WithPrefix()); err != nil {
 		fmt.Print(err)
 		return
 	} else {
-		fmt.Println("Revision:", putResp)
-		if putResp.PrevKv != nil {
-			fmt.Println("Prevalue:", string(putResp.PrevKv.Value))
+		for _, value := range getResp.Kvs {
+			fmt.Println(value)
 		}
 	}
+
 }
