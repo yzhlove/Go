135923302cb58e41e719f7d88515d63a8210e946
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-04-14 16:03:31 +0800

    test

--- action.com/src/redis_examp/chat08/main.go
+++ action.com/src/redis_examp/chat08/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,19 +13,31 @@ func main() {
 	var (
 		sourceFile, newFile *os.File
 		err                 error
-		sourcePath, newPath string
+		sourcePath          = "/Users/love/redis_filter.txt"
+		bytes               int64
 	)
 	if sourceFile, err = os.Open(sourcePath); err != nil {
 		panic(err)
 	}
 	defer sourceFile.Close()
 
-	if newFile, err = os.Open(newPath); err != nil {
+	if newFile, err = os.Create(sourcePath + "_copy"); err != nil {
 		panic(err)
 	}
 
 	defer newFile.Close()
 
-	//
+	if bytes, err = io.Copy(newFile, sourceFile); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("copy bytes = ", bytes)
+
+	//将文件刷写到硬盘
+	if err = newFile.Sync(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("文件刷写成功")
 
 }
