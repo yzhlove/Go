a55a9948bf17fc045a741d96e6678ef99363885d
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-04-17 00:56:30 +0800

    test

--- action.com/src/redis_examp/chat26/main.go
+++ action.com/src/redis_examp/chat26/main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -16,13 +17,14 @@ type Info struct {
 
 var (
 	MasterID string
-	SavelID  string
+	SaveID   string
 )
 
 func main() {
 
 	cmds := []*Info{
 		&Info{Cmd: "docker", Args: []string{"run", "-d", "-p", "6380:6379", "--name", "saved", "redis"}},
+		&Info{Cmd: "docker", Args: []string{"run", "-d", "-p", "6379:6379", "--name", "masted", "--link", "saved:saved", "-v", "/Develop/Docker/Volumes/Redis:/data", "redis"}},
 	}
 	if err := startDocker(cmds); err != nil {
 		panic(err)
@@ -30,13 +32,27 @@ func main() {
 
 	fmt.Println("cmd running successful .")
 
-	fmt.Println(MasterID, " ", SavelID)
+	fmt.Printf("%q %q \n", MasterID, SaveID)
 
+	time.Sleep(time.Second * 15)
+
+	infos := []*Info{
+		&Info{Cmd: "docker", Args: []string{"stop", MasterID}},
+		&Info{Cmd: "docker", Args: []string{"stop", SaveID}},
+		&Info{Cmd: "docker", Args: []string{"rm", "-f", "saved"}},
+		&Info{Cmd: "docker", Args: []string{"rm", "-f", "masted"}},
+	}
+
+	if err := stopDocker(infos); err != nil {
+		fmt.Println("stop docker err: ", err)
+	}
+
+	fmt.Println("Done .")
 }
 
 func startDocker(cmds []*Info) error {
 
-	values := []*string{&MasterID, &SavelID}
+	values := []*string{&MasterID, &SaveID}
 	errMsg := make(chan error)
 	ctx, cancel := context.WithCancel(context.Background())
 
@@ -47,7 +63,7 @@ func startDocker(cmds []*Info) error {
 				errMsg <- err
 				return
 			} else {
-				*values[i] = string(result)
+				*values[i] = strings.TrimRight(string(result), "\n")
 			}
 		}
 	}(cmds, values)
@@ -65,9 +81,27 @@ func startDocker(cmds []*Info) error {
 	return nil
 }
 
-func stopDocker() error {
-
-	return nil
+func stopDocker(cmds []*Info) error {
+	errMsg := make(chan error)
+	ctx, cancel := context.WithCancel(context.Background())
+	go func(infos []*Info) {
+		defer cancel()
+		for i := 0; i < len(infos); i++ {
+			if _, err := run(infos[i]); err != nil {
+				errMsg <- err
+			}
+		}
+	}(cmds)
+	for {
+		select {
+		case <-time.NewTimer(10 * time.Second).C:
+			return fmt.Errorf("stop docker timeout  ...")
+		case <-ctx.Done():
+			return nil
+		case err := <-errMsg:
+			fmt.Println("[ERROR] stop docker err:", err)
+		}
+	}
 }
 
 func run(info *Info) ([]byte, error) {
