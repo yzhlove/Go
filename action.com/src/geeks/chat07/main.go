8c15dd22a93d3e3dd7d03dd47dca4dacceba90ce
花江夏树 <yuzihan@xianjinchaoren.cn>
2019-05-15 14:39:14 +0800

    test

--- action.com/src/geeks/chat07/main.go
+++ action.com/src/geeks/chat07/main.go
@@ -17,4 +17,23 @@ func main() {
 
 	tsStr := ts.Format("2006-01-02 15:04:05")
 	fmt.Println("str => ", tsStr)
+
+	fmt.Println("=============================")
+
+	testTs()
+
+}
+
+func testTs() {
+
+	ts := time.Now().Unix()
+	fmt.Println(ts)
+	str := time.Unix(ts, 0).Format("2006-01-02 15:04:05")
+	fmt.Println(str)
+
+	tsLocal, _ := time.LoadLocation("Local")
+	//t, _ := time.Parse("2006-01-02 15:04:05", str)
+	t, _ := time.ParseInLocation("2006-01-02 15:04:05", str, tsLocal)
+	fmt.Println(t.Unix())
+
 }
